c83eed076b29e2b6c053331818861cae95cb10e5
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2020-04-18 19:27:39 -0600

    Game Clock runs

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -4,7 +4,47 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      gameDate: new Date('January 1, 2020').toDateString()
+      gameDate: '',
+      ticker: null,
+    };
+    this.startGameClock = this.startGameClock.bind(this);
+    this.tick = this.tick.bind(this);
+    this.startTicker = this.startTicker.bind(this);
+    this.stopTicker = this.stopTicker.bind(this);
+  }
+
+  componentDidMount() {
+    this.startGameClock();
+    this.startTicker();
+  }
+
+  startGameClock() {
+    let newState = this.state;
+    newState.gameDate = new Date('January 1, 2020').toDateString();
+    this.setState(newState);
+  }
+
+  tick() {
+    let newState = this.state;
+    const oldDate = Date.parse(newState.gameDate);
+    const oneDay = 86400000;
+    const newDate = new Date(oldDate + oneDay).toDateString();
+    newState.gameDate = newDate;
+    this.setState(newState);
+  }
+
+  startTicker() {
+    let newState = this.state;
+    newState.ticker = setInterval(this.tick, 1000);
+    this.setState(newState);
+  }
+
+  stopTicker() {
+    let newState = this.state;
+    if (newState.ticker) {
+      clearInterval(newState.ticker);
+      newState.ticker = null;
+      this.setState(newState);
     };
   }
 
@@ -17,6 +57,10 @@ class Game extends React.Component {
         <p>{gameDate}</p>
       </div>
     );
+  }
+
+  componentWillUnmount() {
+    this.stopTicker();
   }
 };
 
