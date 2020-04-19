f2bf62629ac99093d4e88b75649a89a9c6301947
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2020-04-18 19:07:06 -0600

    Game date

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameDate: new Date('January 1, 2020').toDateString()
+    };
+  }
+
   render() {
+    const { gameDate } = this.state;
     return (
       <div id="game">
         <h1>Game</h1>
+        <h2>Date</h2>
+        <p>{gameDate}</p>
       </div>
     );
   }
