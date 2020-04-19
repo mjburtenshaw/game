9cd883b25800969d099e15b0a9b0a79792196036
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2020-04-18 20:21:50 -0600

    Can add funds

--- src/containers/Bank.js
+++ src/containers/Bank.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 class Bank extends React.Component {
   constructor(props) {
@@ -12,14 +13,23 @@ class Bank extends React.Component {
         USD: 10000
       }
     };
+    this.credit = this.credit.bind(this);
+  }
+
+  credit({ type, amount }) {
+    let newState = this.state;
+    newState.balances[type] += amount;
+    this.setState(newState);
   }
 
   render() {
     const { currency, balances } = this.state;
+    const fundraiseButton = <Button onClick={event => this.credit({ event, type: currency.type, amount: 100 })}>Raise Funds +100</Button>
     return (
       <div id="bank">
         <h2>Bank</h2>
         <p>{currency.symbol}{balances[currency.type]}</p>
+        {fundraiseButton}
       </div>
     );
   }
